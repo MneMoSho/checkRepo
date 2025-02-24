20e89ce1b0ffbaf4f38ed8be49270830bb595b3d
glebkurylovic@gmail.com <glebkurylovic@gmail.com>
2025-02-24 21:26:36 +0300

    constructor

--- src/main/java/com/example/checkrepo/mapper/FlightMapper.java
+++ src/main/java/com/example/checkrepo/mapper/FlightMapper.java
@@ -2,7 +2,6 @@ package com.example.checkrepo.mapper;
 
 import com.example.checkrepo.dto.FlightDto;
 import com.example.checkrepo.entities.Flight;
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
