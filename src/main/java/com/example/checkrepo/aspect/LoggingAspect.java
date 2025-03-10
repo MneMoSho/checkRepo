e36e37040c78dee437c7606780447564523dd08e
glebkurylovic@gmail.com <glebkurylovic@gmail.com>
2025-03-10 20:29:07 +0300

    logBack added

--- src/main/java/com/example/checkrepo/aspect/LoggingAspect.java
+++ src/main/java/com/example/checkrepo/aspect/LoggingAspect.java
@@ -1,9 +1,13 @@
 package com.example.checkrepo.aspect;
 
+import com.example.checkrepo.exception.ObjectNotFoundException;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,27 +15,32 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class LoggingAspect {
     @Pointcut("execution(public * com.example.checkrepo.controller.*.*(..))")
-    public void controllerLog() {}
+    public void controllerLog() {
+    }
 
     @Pointcut("execution(public * com.example.checkrepo.service.*.*(..))")
-    public void serviceLog() {}
+    public void serviceLog() {
+    }
 
-   @AfterReturning(returning = "returnObject", pointcut = "controllerLog()")
+    @AfterReturning(returning = "returnObject", pointcut = "controllerLog()")
     public void afterReturning(Object returnObject) {
         log.info("return value: {}", returnObject);
-   }
+    }
 
-   @After("controllerLog()")
-    public void doAfter(JoinPoint joinPoint) {
-        log.info("method executed succesfully: {}.{}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
-   }
+    @AfterThrowing(pointcut = "controllerLog()", throwing = "exception")
+    public void logError(ObjectNotFoundException exception) {
+        long start = System.currentTimeMillis();
+        long executionTime = System.currentTimeMillis() - start;
+        log.error("Exception thrown {}, {}", exception, executionTime);
+    }
 
-   @Around("controllerLog()")
+    @Around("controllerLog()")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         Object proceed = joinPoint.proceed();
         long executionTime = System.currentTimeMillis() - start;
-        log.info("Execution method: {}.{}. Execution {}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName(), executionTime);
+        log.info("Executed successfully: {}.{}. Execution {}", joinPoint.getSignature()
+                .getDeclaringTypeName(), joinPoint.getSignature().getName(), executionTime);
         return proceed;
-   }
+    }
 }
