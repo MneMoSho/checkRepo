839b12c9da4faefb15baa6bdc1fbcd47869f052f
glebkurylovic@gmail.com <glebkurylovic@gmail.com>
2025-03-25 09:26:13 +0300

    checkStyle

--- src/main/java/com/example/checkrepo/controller/LogController.java
+++ src/main/java/com/example/checkrepo/controller/LogController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/log")
-public class logController {
+public class LogController {
 
     @GetMapping()
     public void newLogFile(@QueryParam("date") String date, @QueryParam("minutes") String minutes) {
