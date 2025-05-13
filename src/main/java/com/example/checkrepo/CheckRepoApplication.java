4bc27189bf963b026a8978f50a58a1949224a7ca
glebkurylovic@gmail.com <glebkurylovic@gmail.com>
2025-05-13 13:21:49 +0300

    docker

--- src/main/java/com/example/checkrepo/CheckRepoApplication.java
+++ src/main/java/com/example/checkrepo/CheckRepoApplication.java
@@ -1,5 +1,6 @@
 package com.example.checkrepo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CheckRepoApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(CheckRepoApplication.class, args);
     }
-
 }
+
